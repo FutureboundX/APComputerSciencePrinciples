d161503f7071fd008113d6ceebf45b680f91c827
FutureboundX <48761556+FutureboundX@users.noreply.github.com>
2020-01-15 08:10:45 -0500

    Create Unit5Hostess.java

---
+++ Unit5Hostess.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Hostess {
+	
+	//Method for greeting people
+	public static void greet(int numOfPeople){
+		System.out.println("Hi, and welcome to Applebees!");
+		System.out.println("I see there are there are " + numOfPeople + " having dinner tonight.");
+		System.out.println("Please follow me to be seated~");
+		
+	}
+}
